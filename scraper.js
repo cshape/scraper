a02b717b38f94319be120c1651a1a06de7af4d73
cremaster <25466659+cshape@users.noreply.github.com>
2020-01-15 12:35:42 -0800

    first commit

---
+++ scraper.js
@@ -0,0 +1,50 @@
+const puppeteer = require('puppeteer');
+const $ = require('cheerio');
+const ObjectsToCsv = require('objects-to-csv');
+const url = 'https://www.nectarsleep.com/faq';
+
+let intentList = [{category: 'Category Path', 
+				   question: 'Intent Name', 
+				   variant: 'Visitor Questions',
+				   answer: 'Response'}];
+
+let questions = [];
+let answers = [];
+let newArray = [];
+
+puppeteer
+  .launch()
+  .then(function(browser) {
+    return browser.newPage();
+  })
+  .then(function(page) {
+    return page.goto(url).then(function() {
+      return page.content();
+    });
+  })
+  .then(function(html) {
+    $('.togglesWrap_Zsi > article > p', html).each(function() {
+      questions.push($(this).text());
+      // console.log($(this).text());
+      });
+    $('.togglesWrap_Zsi > article > div > div', html).each(function() {
+      answers.push($(this).text());
+      // console.log($(this).text());
+      });
+    })
+  	.then(function() {
+  		for (var x = 0; x < questions.length; x++) {
+	   	let thing = {
+	   		question: questions[x],
+	   		answer: answers[x]
+	   	}
+	   	newArray.push(thing);
+	 }
+	 intentList.push(...newArray);
+	 console.log(intentList);
+	 let csv = new ObjectsToCsv(intentList);
+     csv.toDisk('./intent_list.csv');
+  	})
+  
+
+
